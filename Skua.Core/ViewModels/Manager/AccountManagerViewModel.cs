7d4c656eeeb2872a51564edd65cf8c24893a4728
SharpTheNightmare <koen.t04@gmail.com>
2025-10-14 04:00:51 -0500

    i forgor a t in not

--- Skua.Core/ViewModels/Manager/AccountManagerViewModel.cs
+++ Skua.Core/ViewModels/Manager/AccountManagerViewModel.cs
@@ -98,7 +98,7 @@ public sealed partial class AccountManagerViewModel : BotControlViewModelBase
 
         if (string.IsNullOrEmpty(DisplayNameInput) && !UseNameAsDisplay)
         {
-            _dialogService.ShowMessageBox("Display name must not be empty when \"Username as Display\" is no checked", "Missing Input");
+            _dialogService.ShowMessageBox("Display name must not be empty when \"Username as Display\" is not checked", "Missing Input");
             return;
         }
 
