f39efb4033d42ff3258dc1bdbc0f7836948db6ab
blackarrow123 <dev@jcsawyer.me>
2026-01-24 23:31:43 +0000

    update account tab

---
+++ Skua.WPF/UserControls/GroupItemUserControl.xaml.cs
@@ -0,0 +1,29 @@
+using System.Windows;
+using System.Windows.Controls;
+using CommunityToolkit.Mvvm.Messaging;
+using Skua.Core.Messaging;
+using Skua.Core.ViewModels;
+using Skua.Core.ViewModels.Manager;
+
+namespace Skua.WPF.UserControls;
+
+/// <summary>
+/// Interaction logic for GroupItemUserControl.xaml
+/// </summary>
+public partial class GroupItemUserControl : UserControl
+{
+    public GroupItemUserControl()
+    {
+        InitializeComponent();
+    }
+
+    private void RemoveAccountFromGroup_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button &&
+            button.DataContext is AccountItemViewModel account &&
+            DataContext is GroupItemViewModel group)
+        {
+            WeakReferenceMessenger.Default.Send(new RemoveAccountFromGroupMessage(account, group));
+        }
+    }
+}
