762f34f17588f722a66b054e0490a29b950c2059
blackarrow123 <dev@jcsawyer.me>
2026-02-03 00:19:06 +0000

    feat(accounts): add groups to settings and update messaging

--- Skua.Core/Messaging/ManagerMesssages.cs
+++ Skua.Core/Messaging/ManagerMesssages.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.Messaging.Messages;
 using Skua.Core.Models.GitHub;
 using Skua.Core.ViewModels;
+using Skua.Core.ViewModels.Manager;
 
 namespace Skua.Core.Messaging;
 
@@ -15,3 +16,10 @@ public sealed record UpdateFinishedMessage();
 public sealed record ClearPasswordBoxMessage();
 public sealed record RemoveAccountMessage(AccountItemViewModel Account);
 public sealed record AccountSelectedMessage(bool Add);
+public sealed record AddAccountToGroupMessage(AccountItemViewModel Account);
+public sealed record AddTagsMessage(AccountItemViewModel Account);
+public sealed record StartAccountMessage(AccountItemViewModel Account, bool WithScript);
+public sealed record RemoveGroupMessage(GroupItemViewModel Group);
+public sealed record StartGroupMessage(GroupItemViewModel Group, bool WithScript);
+public sealed record RenameGroupMessage(GroupItemViewModel Group);
+public sealed record RemoveAccountFromGroupMessage(GroupItemViewModel Group, AccountItemViewModel Account);
